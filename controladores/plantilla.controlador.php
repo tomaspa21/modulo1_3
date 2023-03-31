c8c2c66eaff219f62a82224ad8d5496a1922f4d3
tomaspa21 <palaciostomas73@gmail.com>
2023-03-31 17:23:51 -0300

    modulo1_3

---
+++ controladores/plantilla.controlador.php
@@ -0,0 +1,7 @@
+<?php
+
+class ControladorPlantilla{
+    public function ctrGetPlantilla(){
+        include "vistas/plantilla.php";
+    }
+}
---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+require_once "controladores/plantilla.controlador.php";
+
+
+//Instanciar objeto
+$plantilla = new ControladorPlantilla();
+//Ejecutar Metodo
+$plantilla ->ctrGetPlantilla();
